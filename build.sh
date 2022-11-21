62614a829a142c834c410eeab7c2e3c5bad049a0
Patryk Koscik <pkoscik@internships.antmicro.com>
2022-11-21 16:17:14 +0100

    Add MuPDF SIGHUP emitting upon a finished build

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 ARGS=""
+UPDATE=false
 
 usage() {
   echo -e "Avabile flags:"
   echo -e "   -b: builds a .pdf file"
   echo -e "   -c: performs a \"small\" clean-up"
   echo -e "   -C: performs a \"big\" clean-up" 
+  echo -e "   -u: Send a SIGHUP signal to all MuPDF processes after compilation"
 }
 
 # Check if any argument was passed
@@ -17,12 +19,13 @@ if [ $# -eq 0 ]
 fi
 
 # Parse flags
-while getopts bcCh flag 2>/dev/null
+while getopts bcChu flag 2>/dev/null
 do
   case "${flag}" in
     b) ARGS+="-pdf";;
     c) ARGS+="-c";;
     C) ARGS+="-C";;
+    u) UPDATE=true;;
     h)
       echo "A small script to streamline the usage of texlive docker image."
       usage
@@ -38,3 +41,10 @@ done
 
 # Run texlive image
 sudo docker run -v $PWD:/files -w /files texlive/texlive latexmk ${ARGS}
+
+# Send a update signall to all MuPDF instances
+if $UPDATE; then
+    echo "Sending a SIGHUP to all MuPDF instances!"
+    pkill -SIGHUP mupdf
+fi
+  
